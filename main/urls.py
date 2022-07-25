73fb697b60c08a68e338769acf3343e7ba792d4c
Timofei Ryko <timofei.ryko@gmail.com>
2022-07-25 23:32:07 +0300

    URLs update to sync with pythonanywhere

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.problems, name='index'),
     path('about/', views.problems, name='about'),
-    path('problems/', views.problems, name='problems')
+    path('problems/', views.problems, name='problems'),
+    path('update_server/', views.update, name="update")
 ]
