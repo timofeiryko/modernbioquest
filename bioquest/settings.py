2bee0d397e9cd6af4dbbfc94a5e210967e67340e
Timofei Ryko <timofei.ryko@gmail.com>
2023-02-28 00:27:55 +0200

    dropbox token fix

--- bioquest/settings.py
+++ bioquest/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-DROPBOX_OAUTH2_TOKEN = str(os.getenv('DROPBOX_OAUTH2_TOKEN'))
+DROPBOX_OAUTH2_REFRESH_TOKEN = str(os.getenv('DROPBOX_OAUTH2_TOKEN'))
 DROPBOX_APP_KEY = str(os.getenv('DROPBOX_APP_KEY'))
 DROPBOX_APP_SECRET = str(os.getenv('DROPBOX_APP_SECRET'))
 
